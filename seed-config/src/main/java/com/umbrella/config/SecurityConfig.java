2154f3a1538c71196ab93e181e599c26e7bef515
CloudSen <www.402645063@gmail.com>
2018-01-31 17:17:47 +0800

    - 添加笔记seed

--- seed-config/src/main/java/com/umbrella/config/SecurityConfig.java
+++ seed-config/src/main/java/com/umbrella/config/SecurityConfig.java
@@ -20,11 +20,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        /*http
                 .authorizeRequests() // 对请求授权
                 .antMatchers("/index","/login").permitAll()
                 .anyRequest().authenticated()
-                .and().formLogin().loginPage("/login");
+                .and().formLogin().loginPage("/login");*/
     }
 
     @Override
---
+++ seed-web/src/main/java/com/umbrella/controller/RequireJsController.java
@@ -0,0 +1,14 @@
+package com.umbrella.controller;
+
+/**
+ * <p>
+ * Description: 测试requireJs
+ * </p>
+ *
+ * @author: 011096=>yangyunsen@inner.czy.com
+ * @date: 2018-01-31
+ * @version: 1.0
+ */
+public class RequireJsController {
+
+}
