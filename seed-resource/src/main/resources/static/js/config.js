caf90f8145a88a1b3ae19de077ee348b6f58c376
CloudSen <www.402645063@gmail.com>
2018-02-24 18:32:29 +0800

    添加requireJS笔记

--- seed-resource/src/main/resources/static/js/config.js
+++ seed-resource/src/main/resources/static/js/config.js
@@ -1,27 +1,26 @@
-!function () {
-    /** 配置js文件路径 **/
-    require.config({
-        baseUrl: "/js/",
-        paths: {
-            "jquery": ["http://libs.baidu.com/jquery/2.0.3/jquery","jQuery/jquery-3.2.1.min"],
-            "test4": "learnrequirejs/test4",
-            "test5": "learnrequirejs/test5",
-            "test6": "learnrequirejs/test6",
-            "h5sock": "learnsockjs/h5sock",
-            "sockjs": "SockJS/sockjs.min"
-        },
-        shim: {
-            "sockjs": {
-                exports: "SockJS"
-            }
+/** 配置js文件路径 **/
+require.config({
+    baseUrl: "/js/",
+    paths: {
+        "jquery": ["http://libs.baidu.com/jquery/2.0.3/jquery","jQuery/jquery-3.2.1.min"],
+        "test4": "learnrequirejs/test4",
+        "test5": "learnrequirejs/test5",
+        "test6": "learnrequirejs/test6",
+        "h5sock": "learnsockjs/h5sock",
+        "sockjs": "SockJS/sockjs.min"
+    },
+    shim: {
+        "sockjs": {
+            exports: "h5sock"
         }
-    });
+    }
+});
 
-    /** 使用requireJS加载JS文件 **/
-    /*require(["jquery","test4","test5","test6"],() => {
-        console.log("js加载完毕");
-    });*/
-    require(["h5sock","sockjs"],function () {
-        console.log("js加载完毕");
-    });
-}();
+/** 使用requireJS加载JS文件 **/
+require(["jquery","test4","test5","test6"],() => {
+    console.log("js加载完毕");
+});
+/*require(["jquery","h5sock","sockjs"],function ($,h5sock,sockjs) {
+    console.log("js加载完毕");
+    h5sock.h5sock();
+});*/
--- seed-resource/src/main/resources/static/js/learnsockjs/h5sock.js
+++ seed-resource/src/main/resources/static/js/learnsockjs/h5sock.js
@@ -1,56 +1,61 @@
-define(function () {
-    var websocket = establishConnection();
-    window.onbeforeunload = function (ev) {
-        closeWebSocket();
+define(["jquery"],function ($) {
+    var h5sock = function ($) {
+        var websocket = establishConnection();
+        window.onbeforeunload = function (ev) {
+            closeWebSocket();
+        };
+
+        function closeWebSocket() {
+            if (websocket !== null) {
+                websocket.close();
+            } else {
+                alert("连接为空");
+            }
+        }
+
+        function sendMessage() {
+            if (websocket.readyState === 3) {
+                alert("您已断线，请重新连接。");
+            } else {
+                websocket.send($("#text").val());
+            }
+        }
+
+        function reconnection() {
+            websocket = establishConnection();
+        }
+
+        function establishConnection() {
+            var websocket = null;
+            var $content = $('#content');
+            $content.empty();
+            //判断当前浏览器是否支持WebSocket
+            if ('WebSocket' in window) {
+                websocket = new WebSocket("ws://localhost:7001/websocket");
+            } else {
+                var $p = $("<p>当前浏览器不支持 websocket</p>");
+                $p.appendTo($content);
+            }
+            websocket.onopen = function (ev) {
+                var $p = $("<p>socket连接建立</p>");
+                $p.appendTo($content);
+            };
+            websocket.onclose = function (ev) {
+                var $p = $("<p>socket连接关闭</p>");
+                $p.appendTo($content);
+            };
+            websocket.onmessage = function (ev) {
+                var $p = $("<p>" + ev.data + "</p>");
+                $p.appendTo($content);
+            };
+            websocket.onerror = function (ev) {
+                var $p = $("<p>websocket发生错误</p>");
+                $p.appendTo($content);
+            };
+            return websocket;
+        }
     };
-
-    function closeWebSocket() {
-        if (websocket !== null) {
-            websocket.close();
-        } else {
-            alert("连接为空");
-        }
-    }
-
-    function sendMessage() {
-        if (websocket.readyState === 3) {
-            alert("您已断线，请重新连接。");
-        } else {
-            websocket.send($("#text").val());
-        }
-    }
-
-    function reconnection() {
-        websocket = establishConnection();
-    }
-
-    function establishConnection() {
-        var websocket = null;
-        var $content = $('#content');
-        $content.empty();
-        //判断当前浏览器是否支持WebSocket
-        if ('WebSocket' in window) {
-            websocket = new WebSocket("ws://localhost:7001/websocket");
-        } else {
-            var $p = $("<p>当前浏览器不支持 websocket</p>");
-            $p.appendTo($content);
-        }
-        websocket.onopen = function (ev) {
-            var $p = $("<p>socket连接建立</p>");
-            $p.appendTo($content);
-        };
-        websocket.onclose = function (ev) {
-            var $p = $("<p>socket连接关闭</p>");
-            $p.appendTo($content);
-        };
-        websocket.onmessage = function (ev) {
-            var $p = $("<p>" + ev.data + "</p>");
-            $p.appendTo($content);
-        };
-        websocket.onerror = function (ev) {
-            var $p = $("<p>websocket发生错误</p>");
-            $p.appendTo($content);
-        };
-        return websocket;
+    return {
+        h5sock: h5sock
     }
 });
