4799f6c3d05a2698b5c6d16885fd4704ceb8de88
CloudSen <www.402645063@gmail.com>
2018-05-31 01:29:16 +0800

    删除无用import

--- seed-config/src/main/java/com/umbrella/interceptor/TestWebSocketInterception.java
+++ seed-config/src/main/java/com/umbrella/interceptor/TestWebSocketInterception.java
@@ -1,6 +1,5 @@
 package com.umbrella.interceptor;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.server.ServerHttpRequest;
