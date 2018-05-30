58da990aaad24a54923b22589242bca64d6d8d21
CloudSen <www.402645063@gmail.com>
2018-05-31 02:21:00 +0800

    修改require

--- seed-resource/src/main/resources/static/js/config.js
+++ seed-resource/src/main/resources/static/js/config.js
@@ -2,7 +2,7 @@
 require.config({
     baseUrl: "/js/",
     paths: {
-        "jquery": ["http://libs.baidu.com/jquery/2.0.3/jquery","jQuery/jquery-3.2.1.min"],
+        "jquery": ["http://libs.baidu.com/jquery/3.2.1/jquery","jQuery/jquery-3.2.1.min"],
         "test4": "learnrequirejs/test4",
         "test5": "learnrequirejs/test5",
         "test6": "learnrequirejs/test6",
@@ -17,10 +17,10 @@ require.config({
 });
 
 /** 使用requireJS加载JS文件 **/
-require(["jquery","test4","test5","test6"],() => {
-    console.log("js加载完毕");
-});
-/*require(["jquery","h5sock","sockjs"],function ($,h5sock,sockjs) {
+// require(["jquery","test4","test5","test6"],() => {
+//     console.log("js加载完毕");
+// });
+require(["jquery","h5sock","sockjs"],function ($,h5sock,sockjs) {
     console.log("js加载完毕");
     h5sock.h5sock();
-});*/
+});
