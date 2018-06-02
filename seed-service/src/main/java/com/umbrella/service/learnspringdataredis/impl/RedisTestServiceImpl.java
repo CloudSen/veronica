15af9f6652089158cc55b31b57338754ce8f14d0
CloudSen <www.402645063@gmail.com>
2018-06-02 18:47:49 +0800

    新增Redis测试服务实现类

---
+++ seed-service/src/main/java/com/umbrella/service/learnspringdataredis/impl/RedisTestServiceImpl.java
@@ -0,0 +1,52 @@
+package com.umbrella.service.learnspringdataredis.impl;
+
+import com.umbrella.service.learnspringdataredis.RedisTestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * @author CloudSen
+ * @date 2018-6-1
+ * @apiNote Spring Data Redis -- String 数据操作
+ */
+@Service
+public class RedisTestServiceImpl implements RedisTestService {
+
+    private RedisTemplate<String, String> stringRedisTemplate;
+    private ValueOperations<String, String> opsForString;
+
+    @Autowired
+    public RedisTestServiceImpl(RedisTemplate<String, String> stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.opsForString = this.stringRedisTemplate.opsForValue();
+    }
+
+    @Override
+    public void add(String key, String value) {
+        this.opsForString.set(key, value);
+    }
+
+    @Override
+    public void delete(String key) {
+        this.stringRedisTemplate.delete(key);
+    }
+
+    @Override
+    public void update(String key, String value) {
+        this.opsForString.getAndSet(key, value);
+    }
+
+    @Override
+    public Set<String> findAll() {
+        return this.stringRedisTemplate.keys("*");
+    }
+
+    @Override
+    public String findByKey(String key) {
+        return this.opsForString.get(key);
+    }
+}
