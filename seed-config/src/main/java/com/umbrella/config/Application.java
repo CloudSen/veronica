7f1def9d7011ad532ace11c6c7da08f2dbf1ebd8
CloudSen <www.402645063@gmail.com>
2018-06-02 18:43:05 +0800

    修改注解

--- seed-config/src/main/java/com/umbrella/config/Application.java
+++ seed-config/src/main/java/com/umbrella/config/Application.java
@@ -3,22 +3,21 @@ package com.umbrella.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
  * Description: Spring Boot 启动类
- * @RestController是@ResponseBody和@Controller的组合注解
+ * 注解SpringBootApplication中的scanBasePackages参数相当于ComponentScan注解，配置了自动扫描。
  * </p>
  *
  * @author: 011096=>yangyunsen@inner.czy.com
  * @date: 2018-01-09
  * @version: 1.0
  */
-@SpringBootApplication
-@RestController
-@ComponentScan({"com.umbrella"})
+@SpringBootApplication(
+        scanBasePackages = {"com.umbrella"},
+        exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
+        )
 @MapperScan("com.umbrella.mapper")
 public class Application {
     public static void main(String[] args) {
